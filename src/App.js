96019437d36b906b7f4a1e450249bb9ded42a4e6
Josh Levinson <joshua.levinson@yale.edu>
2019-10-25 14:48:44 -0500

    Playing around with some more React stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
 import React from 'react';
 import './App.css';
 
+
+function Greeting(props) {
+  return (
+    <div>
+      Hello, my name is {props.name}.
+    </div>
+  );
+}
+
+function ComedyShow(props) {
+  return (
+    <div className="show">
+      <div className="showTitle">
+        {props.title}
+      </div>
+      <div className="showTime">
+        {props.startTime}
+      </div>
+      <div className="showPrice">
+        {props.showPrice}
+      </div>
+    </div>
+  );
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <h3>It is {this.state.date.toLocaleTimeString()}.</h3>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Welp, this is it.
-        </p>
+        <Greeting name="Josh Levinson" />
       </header>
+      <Clock />
+      <div>
+        <ComedyShow title="Josh's Comedy Show" startTime="8:00pm" showPrice="$8.00" />
+      </div>
     </div>
   );
 }
